07aba450519f65bd8a5ca8809c5ab68fc01d49d1
Khalotis11 <moekhaloti@gmail.com>
2019-08-08 19:00:54 +0100

    Added post request, we can now add new users

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const db = require("./data/db.js");
 //Global objects
 const server = express();
 
-//Request handlers
+//middleware
+server.use(express.json());
+//Request handlers:
+
 //GET
 server.get("/api/users", (req, res) => {
   db.find()
@@ -16,6 +19,21 @@ server.get("/api/users", (req, res) => {
     .catch(err => {
       res.status(500).json({
         err: "The users information could not be retrieved."
+      });
+    });
+});
+
+//POST
+server.post("/api/users", (req, res) => {
+  const newHub = req.body;
+  //validate the users
+  db.insert(newHub)
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "Please provide name and bio for the user."
       });
     });
 });
