ab180f93a153bd2c97d56786217536992e35cc0c
Khalotis11 <moekhaloti@gmail.com>
2019-08-08 02:28:09 +0100

    Added a GET Request handler

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // implement your API here
+//libraries
 const express = require("express");
-
+//Other files
+const db = require("./data/db.js");
+//Global objects
 const server = express();
+
+//Request handlers
+//GET
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: "The users information could not be retrieved."
+      });
+    });
+});
 
 server.listen(3333, () => {
   console.log("Server is running on port 3333...");
